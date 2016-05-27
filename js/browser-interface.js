06f7aabb4bdddf90174b93f1a38f238ef00953fe
Kevin Deganos <kdeganos-09@sandiego.edu>
2016-05-27 14:14:00 -0700

    allow users to go from a display of github profile info to a display of 30 repos

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,20 +1,38 @@
 var Lookup = require('./../js/lookup.js').Lookup;
-
+var userName;
 $(document).ready(function() {
   var lookup = new Lookup();
-
   $('#userNameButton').click(function() {
-    var userName = $('#userName').val();
+    document.getElementById("profile").innerHTML = "";
+    document.getElementById("links").innerHTML = "";
+    document.getElementById("userOutput").innerHTML = "";
+    document.getElementById("repoTable").innerHTML = "";
+
+    userName = $('#userName').val();
+    lookup.getUser(userName, displayProfile);
+  });
+
+  var displayProfile = function(fullName, avatar, email, followers) {
+    $('#profile').append("<img src='" + avatar + "'><br> <h3>Name: " + fullName + "</h3><br><h4>Email: " + email + "<br> <h4>Followers: " + followers);
+    $('#links').append("<a id='repos'>View Repositories</a>");
+  };
+
+  $('#links').click(function() {
+    document.getElementById("profile").innerHTML = "";
+    document.getElementById("links").innerHTML = "";
+    document.getElementById("userOutput").innerHTML = "";
+    document.getElementById("repoTable").innerHTML = "";
+    $('#repoTable').append("<tr><th>Repository</th><th>Description</th></tr>");
     lookup.getRepos(userName, displayUser, displayRepos);
   });
 
   var displayUser = function(userName, avatar) {
-    document.getElementById("userOutput").innerHTML = "";
-
     $('#userOutput').append("<img src='" + avatar + "'><br> <h3>User Name: " + userName + "</h3><br>");
   };
 
   var displayRepos = function(repoName, description) {
-    $('#repoOutput').append("<strong>Repository: </strong>" + repoName + "<br> <strong>Description: </strong>" + description + "<br><br>");
+    // $('#repoOutput').append("<strong>Repository: </strong>" + repoName + "<br> <strong>Description: </strong>" + description + "<br><br>");
+    $('#repoTable').append("<tr><td>" + repoName + "</td><td>" + description + "</td></tr>");
   };
+
 });
--- js/lookup.js
+++ js/lookup.js
@@ -4,25 +4,36 @@ exports.Lookup = function() {
 
 };
 
-// exports.Lookup.prototype.getUser = function(userName, displayRepos) {
-//   $.get('https://api.github.com/users/' + 'kdeganos' + '?access_token=' + apiKey).then(function(response){
-//     // console.log(response);
-//     displayUser(response[0].owner.login, response[0].owner.avatar_url);
-//
-//     displayRepos()
-//   }).fail(function(error){
-//     $('#userOutput').text(error.responseJSON.message);
-//   });
+exports.Lookup.prototype.getUser = function(userName, displayProfile) {
+  $.get('https://api.github.com/users/' + userName + '?access_token=' + apiKey).then(function(response){
+    var fullName = response.name;
+    var email = response.email;
+
+    if (fullName === null) {
+      fullName = "N/A";
+    }
+    if (email === null) {
+      email = "N/A";
+    }
+
+    displayProfile(fullName, response.avatar_url, email, response.followers);
+  }).fail(function(error){
+    $('#userOutput').text(error.responseJSON.message);
+  });
+};
 
 exports.Lookup.prototype.getRepos = function(userName, displayUser, displayRepos) {
-  $.get('https://api.github.com/users/' + 'kdeganos' + '/repos?access_token=' + apiKey).then(function(response){
-    // console.log(response);
+  $.get('https://api.github.com/users/' + userName + '/repos?access_token=' + apiKey).then(function(response){
     displayUser(response[0].owner.login, response[0].owner.avatar_url);
-    console.log(response.length);
 
     for (var i = 0; i < response.length; i++) {
-      displayRepos(response[i].name, response[i].description);
+      var desc = response[i].description;
+      if (desc === null || desc === "") {
+        desc = "N/A";
+      }
+      displayRepos(response[i].name, desc);
     }
+
   }).fail(function(error){
     $('#userOutput').text(error.responseJSON.message);
   });
