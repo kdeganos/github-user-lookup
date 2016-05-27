f3a160b7806d4ac51959e67ed6cb2b4f0a1071db
Kevin Deganos <kdeganos-09@sandiego.edu>
2016-05-27 11:34:58 -0700

    allow lookup of 30 repos of a given github user name

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,23 +1,20 @@
-var apiKey = require('./../.env').apiKey;
 var Lookup = require('./../js/lookup.js').Lookup;
-
-// $(document).ready(function() {
-//   var lookup = new Lookup();
-//   $('#userNameButton').click(function() {
-//     var userName = $('#userName').val();
-//     lookup.getRepos(userName);
-//     console.log(userName);
-//   });
-// });
 
 $(document).ready(function() {
   var lookup = new Lookup();
+
   $('#userNameButton').click(function() {
     var userName = $('#userName').val();
-    $.get('https://api.github.com/users/kdeganos?access_token=' + apiKey).then(function(response){
-      console.log(response);
-    }).fail(function(error){
-      console.log(error.responseJSON.message);
-    });
+    lookup.getRepos(userName, displayUser, displayRepos);
   });
+
+  var displayUser = function(userName, avatar) {
+    document.getElementById("userOutput").innerHTML = "";
+
+    $('#userOutput').append("<img src='" + avatar + "'><br> <h3>User Name: " + userName + "</h3><br>");
+  };
+
+  var displayRepos = function(repoName, description) {
+    $('#repoOutput').append("<strong>Repository: </strong>" + repoName + "<br> <strong>Description: </strong>" + description + "<br><br>");
+  };
 });
--- js/lookup.js
+++ js/lookup.js
@@ -4,10 +4,26 @@ exports.Lookup = function() {
 
 };
 
-exports.Lookup.prototype.getRepos = function(userName){
-  $.get('https://api.github.com/users/kdeganos?access_token=' + apiKey).then(function(response){
-    console.log(response);
+// exports.Lookup.prototype.getUser = function(userName, displayRepos) {
+//   $.get('https://api.github.com/users/' + 'kdeganos' + '?access_token=' + apiKey).then(function(response){
+//     // console.log(response);
+//     displayUser(response[0].owner.login, response[0].owner.avatar_url);
+//
+//     displayRepos()
+//   }).fail(function(error){
+//     $('#userOutput').text(error.responseJSON.message);
+//   });
+
+exports.Lookup.prototype.getRepos = function(userName, displayUser, displayRepos) {
+  $.get('https://api.github.com/users/' + 'kdeganos' + '/repos?access_token=' + apiKey).then(function(response){
+    // console.log(response);
+    displayUser(response[0].owner.login, response[0].owner.avatar_url);
+    console.log(response.length);
+
+    for (var i = 0; i < response.length; i++) {
+      displayRepos(response[i].name, response[i].description);
+    }
   }).fail(function(error){
-    console.log(error.responseJSON.message);
+    $('#userOutput').text(error.responseJSON.message);
   });
 };
